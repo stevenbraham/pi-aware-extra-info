5be6278345afa59f7538367bd999b48c2610ae19
Steven Braham <steven@braham.biz>
2016-11-08 22:41:50 +0100

    readme

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #Install file
 rm patch.py
 rm pi-aware-extra-info.js
-touch patch.py
-touch pi-aware-extra-info.js
+wget https://raw.githubusercontent.com/stevenbraham/pi-aware-extra-info/master/patch.py
+wget https://raw.githubusercontent.com/stevenbraham/pi-aware-extra-info/master/pi-aware-extra-info.js
 chmod 777 patch.py
 chmod 777 pi-aware-extra-info.js
 python patch.py
