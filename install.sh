73d116ad77a8428661475a9e73efa6ccfab6adce
Steven Braham <steven@braham.biz>
2016-11-09 00:26:08 +0100

    proof of concept

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 wget https://raw.githubusercontent.com/stevenbraham/pi-aware-extra-info/master/patch.py
 wget https://raw.githubusercontent.com/stevenbraham/pi-aware-extra-info/master/pi-aware-extra-info.js
 wget https://raw.githubusercontent.com/stevenbraham/pi-aware-extra-info/master/update.sh
+wget https://raw.githubusercontent.com/stevenbraham/pi-aware-extra-info/master/proxy.php
 chmod 777 patch.py
 chmod 777 update.sh
+chmod 777 proxy.php
 chmod 777 pi-aware-extra-info.js
 python patch.py
 
